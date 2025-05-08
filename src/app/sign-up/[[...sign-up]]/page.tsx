f86672dc5580bcfc2687df567298151c7f825dc7
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-05-08 20:10:45 +0545

    clerck

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,7 +0,0 @@
-import {SignUp } from '@clerk/nextjs'
-
-import React from "react";
-
-export default function Page() {
-  return <div className="flex items-center justify-center p-3 mt-10 "><SignUp/></div>
-}  
