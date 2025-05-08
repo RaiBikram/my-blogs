b2956ad74e17da4683d1ba15daa1b505e61fe70b
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-05-08 18:22:20 +0545

    fixes:blog error

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -27,7 +27,7 @@ export default function BlogLayout({
                 href="/"
                 className="flex items-center px-4 py-2 bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-lg font-semibold"
               >
-                Bikram's Blog
+                Bikram&apos;s Blog
               </Link>
             </div>
 
