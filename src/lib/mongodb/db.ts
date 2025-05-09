87db834e2eb6bda61ce5ade52a1374244967a562
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-05-09 22:12:38 +0545

    clerk:fix type

--- src/lib/mongodb/db.ts
+++ src/lib/mongodb/db.ts
@@ -1,33 +1,41 @@
 import mongoose from "mongoose";
 
-// Get MongoDB URI from environment variables
-const MONGO_DB_URI = process.env.MONGO_DB_URI;
+const MONGODB_URI = process.env.MONGO_DB_URI!;
 
-if (!MONGO_DB_URI) {
-  throw new Error("MONGO_DB_URI is not defined in environment variables");
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
-mongoose.set("strictQuery", true);
-
-// Use global caching of mongoose connection
 let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-export const connectToDatabase = async (): Promise<typeof mongoose> => {
-  // If there is an existing connection, return it
-  if (cached.conn) return cached.conn;
-
-  // If no connection is available, create one
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_DB_URI, {
-      dbName: "blogs",
-    });
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  // Wait for the promise to resolve and store the connection
-  cached.conn = await cached.promise;
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
   return cached.conn;
-};
+}
--- src/types/mongoose-global.d.ts
+++ src/types/mongoose-global.d.ts
@@ -1,10 +1,10 @@
-import mongoose from "mongoose";
+/* eslint-disable no-var */
+import { Connection } from "mongoose";
 
-// Declare the global variable `mongoose` with an explicit type
 declare global {
   var mongoose: {
-    conn: mongoose.Mongoose | null;
-    promise: Promise<mongoose.Mongoose> | null;
+    conn: Connection | null;
+    promise: Promise<Connection> | null;
   };
 }
 
