ec8900ef7c4f6cbdb21eab64f577affdfe8462d7
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-05-10 13:55:49 +0545

    clerk:fix type

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -49,8 +49,10 @@ console.log("Boday", body)
     });
   }
 
-  // Do something with the payload
-  // For this guide, you simply log the payload to the console
+if(!evt || !evt.data){
+  console.log("not getting event data")
+  return 
+}
   const { id } = evt?.data;
   const eventType = evt?.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
