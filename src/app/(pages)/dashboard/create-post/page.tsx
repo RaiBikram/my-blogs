7be6c44f922e02377265e661fd1c362dff3a66b9
Bikram Rai <175561381+ItsBikramRai@users.noreply.github.com>
2025-05-11 07:48:26 +0545

    added dashborad

--- src/app/(pages)/dashboard/create-post/page.tsx
+++ src/app/(pages)/dashboard/create-post/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import CreatePost from "@/components/dashboard/create-post"
 import React from 'react'
-
+import { useUser } from "@clerk/nextjs"
 type Props = {}
 
 export default function page({}: Props) {
+  const {isSignedIn,user, isLoaded}=useUser();
+  if(!isLoaded){
+    return null
+  }
   return (
     <div>
         <CreatePost/>
